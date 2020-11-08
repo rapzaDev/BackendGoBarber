e83076fec8fd375ca203f223bfbce9e2725a8deb
Rafael Pérez <rapzadev@gmail.com>
2020-11-08 18:14:03 -0300

    Appling  DDD architecture: infra layer

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
+import uploadConfig from '../../../../config/upload';
 
-import CreateUserService from '../services/CreateUserService';
+import CreateUserService from '../../../../modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
