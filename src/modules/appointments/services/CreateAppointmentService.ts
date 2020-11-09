a184313123e20c7dbe7cfea2358fbd7e0b324dab
Rafael Pérez <rapzadev@gmail.com>
2020-11-09 16:35:31 -0300

    Appling  DDD architecture: Liskov Subst. Principle

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 import AppError from '@shared/errors/AppError';
