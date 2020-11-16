27d1480fb8242c15650f6b19c7a53e7cc316440e
Rafael Pérez <rapzadev@gmail.com>
2020-11-16 00:10:28 -0300

    Appling  DDD architecture: Appling Controllers

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -4,48 +4,23 @@ import { container } from 'tsyringe';
 
 import uploadConfig from '@config/upload';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import CreateUserService from '@modules/users/services/CreateUserService';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-    const { name, email, password } = request.body;
-
-    const createUser = container.resolve(CreateUserService);
-
-    const user = await createUser.execute({
-        name,
-        email,
-        password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-        const { user } = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename,
-        });
-
-        delete user.password;
-        delete user.email;
-
-        return response.json({ user });
-    },
+    userAvatarController.update,
 );
 
 export default usersRouter;
